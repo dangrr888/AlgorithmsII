a1d3e9999cd8dd496096352b9514b272b865ba2f
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2018-05-20 10:42:20 +0100

    Week3: BaseballElimination: Added documentation and test client.

--- Week3/BaseballElimination/src/BaseballElimination.java
+++ Week3/BaseballElimination/src/BaseballElimination.java
@@ -1,9 +1,32 @@
+import edu.princeton.cs.algs4.StdOut;
 
 public class BaseballElimination {
 
   /*
    * @brief Create a baseball division from a specified
-   *   filename in a specified format.
+   *   filename in the following format:
+   *   * Number of teams in the division, n
+   *   * n lines, each containing:
+   *    ** A team name (no internal whitespace characters)
+   *    ** The number of wins
+   *    ** The number of losses
+   *    ** The number of remaining games against each team
+   *         in the division.
+   *
+   *  * e.g.,
+   *    ** 4
+   *    ** Atlanta       83 71  8  0 1 6 1
+   *    ** Philadelphia  80 79  3  1 0 0 2
+   *    ** New_York      78 78  6  6 0 0 0
+   *    ** Montreal      77 82  3  1 2 0 0
+   *
+   *  * n must be >= 1
+   *  * Note that a team may play teams outside the
+   *    division, hence the number of remaining games
+   *    of a particular team may exceed the sum of the
+   *    specified remaining games against teams within
+   *    its division.
+   *
    * @param filename The specified filename.
    */
   public BaseballElimination(String filename) {
@@ -118,5 +141,26 @@ public class BaseballElimination {
     }
 
     throw new UnsupportedOperationException();
+  }
+
+  /*
+   * @brief Test client that reads in a sports division from an
+   *   input file specified in the first CL argument, and prints
+   *   whether each team is mathematically eliminated as well as a
+   *   certificate of elimination for each team that is eliminated.
+   */
+  public static void main(String[] args) {
+    final BaseballElimination division = new BaseballElimination(args[0]);
+    for (String team : division.teams()) {
+      if (division.isEliminated(team)) {
+        StdOut.print(team + " is eliminated by the subset R = {");
+        for (String r : division.certificateOfElimination(team)) {
+          StdOut.print(r + " ");
+        }
+        StdOut.println("}");
+      } else {
+        StdOut.println(team + " is not eliminated.");
+      }
+    }
   }
 }
