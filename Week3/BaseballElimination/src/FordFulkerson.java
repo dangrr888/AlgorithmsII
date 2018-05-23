208446d1aa4167ee6fbf6028489f600cd8052d09
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2018-05-23 09:45:40 +0100

    Week3: BaseballElimination: Started impl of FordFulkerson

---
+++ Week3/BaseballElimination/src/FordFulkerson.java
@@ -0,0 +1,87 @@
+import edu.princeton.cs.algs4.Queue;
+
+/*
+ * Class to determine the maxflow of a specified FlowNetwork by:
+ * * All FlowEdge's in the network have zero flow.
+ * * Find the next augmenting path by finding a path from
+ * * the specified source and target in the residual flow
+ *   network using BFS, say.
+ * * Augment the path by adding the bottleneck residual flow
+ *   to the flow of each edge along the path.
+ * * Repeat the previous two steps until there are no more
+ *   augmenting paths.
+ */
+public class FordFulkerson {
+
+  // data members for finding augmenting path
+  private FlowEdge[] edgeTo;
+  private boolean[] marked;
+  private double flow;
+
+  public FordFulkerson(FlowNetwork G, int source, int target) {
+
+    this.edgeTo = new FlowEdge[G.V()];
+    this.marked = new boolean[G.V()];
+    for (int v = 0; v < G.V(); ++v) {
+      this.edgeTo[v] = null;
+      this.marked[v] = false;
+    }
+
+    this.flow = 0.0;
+
+    while (this.hasAugmentingPath(G, source, target)) {
+      this.augmentPath();
+    }
+  }
+
+  private boolean hasAugmentingPath(FlowNetwork G, int source, int target) {
+    // Perform graph search (BFS) across entire graph (not necessary for
+    // just finding a path, but makes the results from inCut valid).
+
+    final Queue<Integer> q = new Queue<Integer>();
+    q.enqueue(source);
+    this.marked[source] = true;
+    while (!q.isEmpty()) {
+      final int v = q.dequeue();
+      for (FlowEdge edge : G.adj(v)) {
+        // check other end is not marked.
+        final int w = edge.other(v);
+        if (!this.marked[w]) {
+          // Check there is a residual capacity in this edge
+          // from v to w.
+          final double residualCapacity = edge.residualCapacityTo(w);
+          if (residualCapacity > 0.0) {
+            // We have a residual flow along this path.
+            // Add dest vertex to the queue and mark it,
+            // and update edgeTo.
+            q.enqueue(w);
+            this.marked[w] = true;
+            this.edgeTo[w] = edge;
+          }
+        }
+      }
+    }
+
+    // If target is inCut it is accessible from source in residual
+    // network, hence we have an augmenting path.
+    return this.inCut(target);
+  }
+
+  private void augmentPath() {
+    //1. Iterate through augmenting path and deduce bottleneck residual capacity
+    //2. Iterate throw path adding flow from 1 to each edge along the path.
+    //3. Increment this.flow by that residual capacity.
+    throw new UnsupportedOperationException();
+  }
+
+  public double maxFlow() {
+    return this.flow;
+  }
+
+  public boolean inCut(int v) {
+    /*
+     * Is v reachable from s in the residual network along the last augmenting path?
+     */
+    return this.marked[v];
+  }
+}
--- Week3/MaxFlowMinCut/src/FordFulkerson.java
+++ Week3/MaxFlowMinCut/src/FordFulkerson.java
@@ -1,10 +1,10 @@
 
 public class FordFulkerson {
-	
+
 	private boolean marked[]; // marked[v] = true if there is a path in residual network from s to v, false otherwise.
 	private FlowEdge[] edgeTo; // edgeTo[v] is the FlowEdge that connects s to v of the next path in the residual network.
 	private double value; // The value of the flow of the next path from s to t in the residual network.
-	
+
 	public FordFulkerson(FlowNetwork G, int s, int t) {
 		this.marked = new boolean[G.V()];
 		this.edgeTo = new FlowEdge[G.V()];
@@ -15,31 +15,31 @@ public class FordFulkerson {
 
 		// FlowNetwork expected to have flow of all its FlowEdge's equal to zero.
 		this.value = 0.0;
-				
+
 		while (this.hasAugmentingPath(G, s, t)) {
 			// Calculate bottleneck capacity along next augmenting path
 			double bottle = Double.POSITIVE_INFINITY;
 			for (int v = t; v != s; v = this.edgeTo[v].other(v)) {
 				bottle = Math.min(bottle, this.edgeTo[v].residualCapacityTo(v));
 			}
-			
+
 			// Augment the flow by reducing residual capacity of every edge along augmenting path
 			// by the bottleneck capacity (i.e., leaving a single full-forward edge or an empty backward one in the path).
 			for (int v = t; v != s; v = this.edgeTo[v].other(v)) {
 				this.edgeTo[v].addResidualFlowTo(v, bottle);
 			}
-			
+
 			this.value += bottle;
 		}
 	}
-	
+
 	private boolean DFS(FlowNetwork G, int v, int t) {
-		
+
 		// Terminating condition.
 		if (v == t) {
 			return true;
 		}
-		
+
 		// Loop over adjacent edges of v
 		for (FlowEdge edge : G.adj(v)) {
 			// Check that other end of edge is not marked.
@@ -60,11 +60,11 @@ public class FordFulkerson {
 				}
 			}
 		}
-		
+
 		// No path found through v.
 		return false;
 	}
-		
+
 	private boolean hasAugmentingPath(FlowNetwork G, int s, int t) {
 		/*
 		 * find path using DFS from s to t in residual network
@@ -73,20 +73,20 @@ public class FordFulkerson {
 			this.edgeTo[v] = null;
 			this.marked[v] = false;
 		}
-		
+
 		this.marked[s] = true;
 		return DFS(G, s, t);
 	}
-	
-	private double value() {
+
+	public double value() {
 		return this.value;
 	}
-	
+
 	/*
 	 * is v reachable from s in residual network?
 	 */
 	public boolean inCut(int v) {
 		return this.marked[v];
 	}
-	
+
 }
