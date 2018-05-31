39043aa159e0c5406ffb0f74c994f795d00bc729
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2018-05-31 09:49:07 +0100

    Week3: BaseballElimination: Refactoring

--- Week3/BaseballElimination/src/BaseballElimination.java
+++ Week3/BaseballElimination/src/BaseballElimination.java
@@ -9,15 +9,24 @@ import edu.princeton.cs.algs4.FordFulkerson;
 
 public class BaseballElimination {
 
+  // Map of team names to an index
   private final HashMap<String, Integer> map;
-  private final HashSet<Integer>[] R;
+
+  // Array of sets of teams constituting the
+  // certificate of elimination for each team.
+  private final HashSet<String>[] R;
+
   private final int numTeams;
   private final String[] teams;
   private final int[] wins;
   private final int[] losses;
-  private final int[] remaining;
-  private final int[][] remainingInDivision;
 
+  // Overall number of remaining games for each team.
+  private final int[] remaining;
+
+  // The number of games remaining between two teams
+  // in the target division.
+  private final int[][] remainingInDivision;
 
   /*
    * @brief Create a baseball division from a specified
@@ -47,6 +56,7 @@ public class BaseballElimination {
    *
    * @param filename The specified filename.
    */
+  @SuppressWarnings("unchecked")
   public BaseballElimination(String filename) {
 
     // Check input.
@@ -57,23 +67,30 @@ public class BaseballElimination {
     // Initialize scanner
     final Scanner sc = new Scanner(filename);
 
+    // Number of teams
     this.numTeams = sc.nextInt();
     if (this.numTeams < 1) {
       sc.close();
       throw new IllegalArgumentException("#teams must be >= 1");
     }
 
-    // Initialize data members.
+    // Initialize remaining data members.
+
     this.map = new HashMap<String, Integer>();
-    this.R = new HashSet[this.numTeams];
+
+    this.R = (HashSet<String>[]) new HashSet[this.numTeams];
+    for (int i = 0; i < this.numTeams; ++i) {
+      this.R[i] = new HashSet<String>();
+    }
+
     this.teams = new String[this.numTeams];
     this.wins = new int[this.numTeams];
     this.losses = new int[this.numTeams];
     this.remaining = new int[this.numTeams];
+
     this.remainingInDivision = new int[this.numTeams][];
     for (int i = 0; i < this.numTeams; ++i) {
       this.remainingInDivision[i] = new int[this.numTeams];
-      this.R[i] = new HashSet<Integer>();
     }
 
     // Read data for each team.
@@ -81,10 +98,12 @@ public class BaseballElimination {
       this.teams[i] = sc.next();
       this.wins[i] = sc.nextInt();
       this.losses[i] = sc.nextInt();
+
       this.remaining[i] = sc.nextInt();
       for (int j = 0; j < this.numTeams; ++j) {
         this.remainingInDivision[i][j] = sc.nextInt();
       }
+
       this.map.put(this.teams[i], i);
     }
 
@@ -93,53 +112,15 @@ public class BaseballElimination {
 
     // Determine eliminated teams.
     for (int i = 0; i < this.numTeams; ++i) {
+
       // Determine contract of elimination for team i:
 
-      /*
-       * Create the flow network.
-       * Node 0: Source Node
-       * Node 1 <= n <= N*(N-1)/2: Nodes representing games between teams 0-1, 0-2, 0-3, etc (NOT DOUBLE COUNTING)
-       * Node N*(N-1)/2 + 1 <= n <= N*(N-1)/2 + N: Nodes representing each team from 0 to N-1
-       * Node N*(N-1)/2+N+1: Target Node.
-       * Note: N*(N-1)/2+N+2 nodes in total.
-       */
-      final FlowNetwork fn = new FlowNetwork(this.numTeams*(this.numTeams-1)/2+this.numTeams+2);
+      // 1. Check if team is trivially eliminated.
+      this.findTrivialCertificateOfElimination(i);
 
-      int gameNodeIdx = 1;
-      for (int j = 0; j < this.numTeams; ++j) {
-        for (int k = j+1; k < this.numTeams; ++k) {
-
-          /*
-           * Add edges from source node to game nodes.
-           */
-          fn.addEdge(new FlowEdge(0, gameNodeIdx, this.remainingInDivision[j][k], 0.0));
-
-          /*
-           * Add edges from game nodes to team nodes
-           */
-          fn.addEdge(new FlowEdge(gameNodeIdx, this.numTeams*(this.numTeams-1)/2 +1+j, Double.POSITIVE_INFINITY, 0.0));
-          fn.addEdge(new FlowEdge(gameNodeIdx, this.numTeams*(this.numTeams-1)/2 +1+k, Double.POSITIVE_INFINITY, 0.0));
-          ++gameNodeIdx;
-        }
-      }
-
-      /*
-       * Add edges from team nodes to target node
-       */
-      for (int j = 0; j < this.numTeams; ++j) {
-        if (i == j) {
-          // Also set edge from team i to target to 0.
-          fn.addEdge(new FlowEdge(this.numTeams*(this.numTeams-1)/2 + 1 + j,
-              this.numTeams*(this.numTeams-1)/2 + this.numTeams + 1,
-              0.0));
-        } else {
-          // Check team j doesn't trivially eliminate i. If so, then set capacity of edge from team i to target to 0
-          final double capacity = this.wins[i] + this.remaining[i] - this.wins[j];
-          fn.addEdge(new FlowEdge(this.numTeams*(this.numTeams-1)/2 + 1 + j,
-                                  this.numTeams*(this.numTeams-1)/2 + this.numTeams + 1,
-                                  capacity > 0.0 ? capacity : 0.0));
-          }
-      }
+      // 2. Calculate max flow to determine min cut of
+      //  team constituting rest of certificate of elimination.
+      final FlowNetwork fn = createFlowNetwork(i);
 
       final FordFulkerson ff = new FordFulkerson(fn, 0, this.numTeams*(this.numTeams-1)/2+this.numTeams+1);
       for (int j = 0; j < this.numTeams; ++j) {
@@ -147,11 +128,100 @@ public class BaseballElimination {
           continue;
         }
         if (ff.inCut(this.numTeams*(this.numTeams-1)/2 +1+j)) {
-          this.R[i].add(j);
+          this.R[i].add(this.teams[j]);
         }
       }
 
     }
+  }
+
+  private void findTrivialCertificateOfElimination(int i) {
+    for (int j = 0; j < this.numTeams; ++j) {
+      if (this.isTriviallyEliminatedBy(i, j)) {
+        this.R[i].add(this.teams[j]);
+      }
+    }
+  }
+
+  private boolean isTriviallyEliminatedBy(int i, int j) {
+    return this.wins[i] + this.remaining[i] < this.wins[j];
+  }
+
+  /*
+   * @brief Create the flow network for calculating
+   * the max flow targeting the team of the specified
+   * index.
+   * @param i The index of the specified team.
+   */
+  private FlowNetwork createFlowNetwork(int i) {
+
+    /*
+     * Create the flow network.
+     * Node 0: Source Node
+     * Node 1 <= n <= N*(N-1)/2: Nodes representing games between teams 0-1, 0-2, 0-3, etc (NOT DOUBLE COUNTING)
+     * Node N*(N-1)/2 + 1 <= n <= N*(N-1)/2 + N: Nodes representing each team from 0 to N-1
+     * Node N*(N-1)/2+N+1: Target Node.
+     * Note: N*(N-1)/2+N+2 nodes in total.
+     */
+
+    final FlowNetwork fn = new FlowNetwork(this.numTeams*(this.numTeams-1)/2+this.numTeams+2);
+
+    int gameNodeIdx = 1;
+    for (int j = 0; j < this.numTeams; ++j) {
+      for (int k = j+1; k < this.numTeams; ++k) {
+
+        /*
+         * Add edges from source node to game nodes.
+         */
+        fn.addEdge(new FlowEdge(0, gameNodeIdx, this.remainingInDivision[j][k], 0.0));
+
+        /*
+         * Add edges from game nodes to team nodes
+         */
+        fn.addEdge(new FlowEdge(gameNodeIdx, this.numTeams*(this.numTeams-1)/2 +1+j, Double.POSITIVE_INFINITY, 0.0));
+        fn.addEdge(new FlowEdge(gameNodeIdx, this.numTeams*(this.numTeams-1)/2 +1+k, Double.POSITIVE_INFINITY, 0.0));
+        ++gameNodeIdx;
+      }
+    }
+
+    /*
+     * Add edges from team nodes to target node
+     */
+    for (int j = 0; j < this.numTeams; ++j) {
+      if (i == j) {
+        // Also set edge from team i to target to 0.
+        fn.addEdge(new FlowEdge(this.numTeams*(this.numTeams-1)/2 + 1 + j,
+            this.numTeams*(this.numTeams-1)/2 + this.numTeams + 1,
+            0.0));
+      } else {
+        // Check team j doesn't trivially eliminate i. If so, then set capacity of edge from team i to target to 0
+        final double capacity = this.wins[i] + this.remaining[i] - this.wins[j];
+        fn.addEdge(new FlowEdge(this.numTeams*(this.numTeams-1)/2 + 1 + j,
+                                this.numTeams*(this.numTeams-1)/2 + this.numTeams + 1,
+                                capacity > 0.0 ? capacity : 0.0));
+        }
+    }
+
+    return fn;
+  }
+
+  /*
+   * @brief Calculate the max flow for team with index i.
+   * @param i The index of the team to be processed.
+   */
+  private void calculateMaxFlow(int i) {
+
+    final FlowNetwork fn = this.createFlowNetwork(i);
+
+  }
+
+  /*
+   * @brief Return the index of the specified team.
+   * @param team The name of the specified team.
+   * @return The index of the specified team.
+   */
+  private int teamIndex(String team) {
+    return this.map.get(team);
   }
 
   /*
@@ -188,7 +258,7 @@ public class BaseballElimination {
       throw new IllegalArgumentException("Null argument.");
     }
 
-    return this.wins[this.map.get(team)];
+    return this.wins[this.teamIndex(team)];
   }
 
   /*
@@ -202,7 +272,7 @@ public class BaseballElimination {
       throw new IllegalArgumentException("Null argument.");
     }
 
-    return this.losses[this.map.get(team)];
+    return this.losses[this.teamIndex(team)];
   }
 
   /*
@@ -216,7 +286,7 @@ public class BaseballElimination {
       throw new IllegalArgumentException("Null argument.");
     }
 
-    return this.remaining[this.map.get(team)];
+    return this.remaining[this.teamIndex(team)];
   }
 
   /*
@@ -236,7 +306,7 @@ public class BaseballElimination {
       throw new IllegalArgumentException("Null argument.");
     }
 
-    return this.remainingInDivision[this.map.get(team1)][this.map.get(team2)];
+    return this.remainingInDivision[this.teamIndex(team1)][this.teamIndex(team2)];
   }
 
   /*
@@ -249,7 +319,7 @@ public class BaseballElimination {
       throw new IllegalArgumentException("Null argument.");
     }
 
-    return !this.R[this.map.get(team)].isEmpty();
+    return !this.R[this.teamIndex(team)].isEmpty();
   }
 
   /*
@@ -264,10 +334,6 @@ public class BaseballElimination {
       throw new IllegalArgumentException("Null argument.");
     }
 
-    final Stack<String> st = new Stack<String>();
-    for (int i : this.R[this.map.get(team)]) {
-      st.push(this.teams[i]);
-    }
-    return st;
+    return this.R[this.teamIndex(team)];
   }
 }
