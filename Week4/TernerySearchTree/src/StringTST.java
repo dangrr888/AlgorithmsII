66128e33c2b4fefa529061871918391b8464e7dd
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2018-06-24 10:58:03 +0100

    Week4: TST: Implemented inorder traversal + augmented with optional use of prefix, longestPrefixOf and wildcards.

--- Week4/TernerySearchTree/src/StringTST.java
+++ Week4/TernerySearchTree/src/StringTST.java
@@ -1,26 +1,28 @@
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class StringTST<T> {
 
 	private Node root = null;
 	
 	private static class Node {
-		public Object val = null;
-		public char key; 
-		public Node[] next = new Node[3];
-	}
-	
-	public StringTST() {
+		Object val = null;
+		char key; 
+		Node[] next = new Node[3];
 	}
 	
 	private Node put(Node x, String key, T val, int d) {
 
+		// Retrieve dth character of key.
 		final char c = key.charAt(d);
 
 		if (x == null) {
+			// Null Node, assign Node key to c.
 			x = new Node();
 			x.key = c;
 		}
 
+		// Compare Node key and c and recurse.
 		if (c < x.key) {
 			x.next[0] = this.put(x.next[0], key, val, d);
 		} else if (c > x.key) {
@@ -34,17 +36,24 @@ public class StringTST<T> {
 		return x;
 	}
 	
+	// Wrapper
 	public void put(String key, T val) {
 		this.root = this.put(this.root, key, val, 0);
 	}
 
 	private Node get(Node x, String key, int d) {
+		
 		if (x == null) {
+			// TST doesn't contain key.
 			return null;
 		}
 		
+		// Retrieve dth character of key
 		final char c = key.charAt(d);
+		
+		// Return the terminating Node of the associated key.
 		Node ret = null;
+		
 		if (c < x.key) {
 			ret = this.get(x.next[0], key, d);
 		} else if (c > x.key) {
@@ -58,6 +67,7 @@ public class StringTST<T> {
 		return ret;
 	}
 	
+	// Wrapper
 	public T get(String key) {
 		if (key == null || key.length() == 0) {
 			return null;
@@ -84,18 +94,27 @@ public class StringTST<T> {
 	}
 	
 	private Node delete(Node x, String key, int d) {
+		
 		if (x == null) {
+			// TST doesn't contain key.
 			return null;
 		}
 		
+		// Retrieve dth character of key.
 		final char c = key.charAt(d);
+		
+		// Compare and recurse.
 		if (c < x.key) {
 			x.next[0] = this.delete(x.next[0], key, d);
 		} else if (c > x.key) {
 			x.next[2] = this.delete(x.next[2], key, d);
 		} else if (d == key.length() - 1) {
-			x.val = null;
+			// Found key matching key.
+			x.val = null; // delete value.
 			if (!hasChildren(x)) {
+				// Node terminating key has no
+				// children hence can delete
+				// without effecting other keys.
 				x = null;
 			}
 		} else {
@@ -105,7 +124,166 @@ public class StringTST<T> {
 		return x;
 	}
 	
+	// Wrapper
 	public void delete(String key) {
 		this.root = this.delete(this.root, key, 0);	
+	}
+	
+	private String print(LinkedList<Character> ll) {
+		final StringBuilder sb = new StringBuilder();
+		for (char c : ll) {
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+	
+	private boolean isMatching(String s1, String s2) {
+		if (s1 == null || s2 == null) {
+			return false;
+		}
+		
+		final int l = s1.length();
+		if (l != s2.length()) {
+			return false;
+		}
+		
+		for (int i = 0; i < l; ++i) {
+			final char ch1 = s1.charAt(i);
+			final char ch2 = s2.charAt(i);
+			
+			if (ch1 == '.' || ch2 == '.') {
+				continue;
+			} else if (ch1 != ch2) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private void prefixProcess(String key, String prefix, Stack<String> st) {
+		if (key.startsWith(prefix)) {
+			st.push(key);
+		}
+	}
+
+	private void longestPrefixProcess(String key, String longestPrefix, Stack<String> st) {
+		if (longestPrefix.startsWith(key)) {
+			if (st.isEmpty()) {
+				st.push(key);
+			} else if (key.length() > st.peek().length()) {
+				st.pop();
+				st.push(key);
+			}
+		}
+	}
+
+	private void wildCardProcess(String key, String wildCard, Stack<String> st) {
+		if (this.isMatching(key, wildCard)) {
+			st.push(key);
+		}
+	}
+
+	private void allKeysProcess(String key, Stack<String> st) {
+		st.push(key);
+	}
+
+	private void inOrderTraversalPrefix( Node x
+			                           , LinkedList<Character> ll
+			                           , Stack<String> st
+			                           , boolean usePrefix
+			                           , String prefix
+			                           , boolean useWildCard
+			                           , String wildCard
+			                           , boolean useLongestPrefix
+			                           , String longestPrefix
+			                           )
+	{
+		
+		if (x == null) {
+			return;
+		}
+		
+		// 1. Add key of current Node
+		ll.add(x.key);
+
+		if (x.val != null) {	
+			// 2. Found key. Determine if key should be added to stack.			
+			final String key = this.print(ll);
+			if (usePrefix) {
+				this.prefixProcess(key, prefix, st);
+			} else if (useLongestPrefix) {
+				this.longestPrefixProcess(key, longestPrefix, st);
+			} else if (useWildCard) {
+				this.wildCardProcess(key, wildCard, st);
+			} else {
+				this.allKeysProcess(key, st);
+			}
+		}
+		
+		// 3. Traverse left.
+		ll.removeLast();
+		this.inOrderTraversalPrefix(x.next[0], ll, st, usePrefix, prefix, useWildCard, wildCard, useLongestPrefix, longestPrefix);
+		
+		// 4. Traverse middle.
+		ll.add(x.key);
+		this.inOrderTraversalPrefix(x.next[1],ll, st, usePrefix, prefix, useWildCard, wildCard, useLongestPrefix, longestPrefix);
+		ll.removeLast();
+		
+		// 5. Traverse right.
+		this.inOrderTraversalPrefix(x.next[2], ll, st, usePrefix, prefix, useWildCard, wildCard, useLongestPrefix, longestPrefix);
+	}
+	
+	public Iterable<String> keys() {
+		if (this.root == null) {
+			return null;
+		}
+		
+		final LinkedList<Character> ll = new LinkedList<Character>();
+		final Stack<String> st = new Stack<String>();
+		this.inOrderTraversalPrefix(this.root, ll, st, false, null, false, null, false, null);
+		return st;
+	}
+	
+	/*
+	 * Return the keys that possesses a prefix equal to s.
+	 */
+	Iterable<String> keysWithPrefix(String prefix) {
+		if (this.root == null) {
+			return null;
+		}
+		
+		final LinkedList<Character> ll = new LinkedList<Character>();
+		final Stack<String> st = new Stack<String>();
+		this.inOrderTraversalPrefix(this.root, ll, st, true, prefix, false, null, false, null);
+		return st;
+	}
+	
+	/*
+	 * Keys that match the search string s (where '.' is a wildCard)
+	 */
+	Iterable<String> keysThatMatch(String wildCard) {
+		if (this.root == null) {
+			return null;
+		}
+		
+		final LinkedList<Character> ll = new LinkedList<Character>();
+		final Stack<String> st = new Stack<String>();
+		this.inOrderTraversalPrefix(this.root, ll, st, false, null, true, wildCard, false, null);
+		return st;
+	}
+
+	/*
+	 * @brief Return the longest key that is a prefix of longestPrefix.
+	 */
+	String longestPrefixOf(String longestPrefix) {
+		if (this.root == null) {
+			return null;
+		}
+		
+		final LinkedList<Character> ll = new LinkedList<Character>();
+		final Stack<String> st = new Stack<String>();
+		this.inOrderTraversalPrefix(this.root, ll, st, false, null, false, null, true, longestPrefix);
+		return st.isEmpty() ? null : st.pop();
 	}
 }
--- Week4/TernerySearchTree/src/TernerySearchTreeTest.java
+++ Week4/TernerySearchTree/src/TernerySearchTreeTest.java
@@ -1,5 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 import org.junit.jupiter.api.Test;
 
 class TernerySearchTreeTest {
@@ -77,5 +80,98 @@ class TernerySearchTreeTest {
 
 		st.delete("qux");
 		assertFalse(st.contains("aux"));
+	}
+	
+	@Test
+	void constructStringTSTAndGetAllKeys() {
+		final StringTST<Integer> st = new StringTST<Integer>();
+		st.put("foo", 0);
+		st.put("bar", 1);
+		st.put("baz", 2);
+		st.put("qux", 3);
+		
+		final ArrayList<String> al = new ArrayList<String>();
+		for (String key : st.keys()) {
+			al.add(key);
+		}
+		
+		assertEquals(al.size(), 4);
+		assertEquals(al.get(0), "bar");
+		assertEquals(al.get(1), "baz");
+		assertEquals(al.get(2), "foo");
+		assertEquals(al.get(3), "qux");
+	}
+	
+	@Test
+	void constructStringTSTAndGetAllKeysWithPrefix() {
+		final StringTST<Integer> st = new StringTST<Integer>();
+		st.put("foo", 0);
+		st.put("foobar", 1);
+		st.put("fab", 2);
+		st.put("fool", 3);
+		st.put("find", 3);
+		
+		final ArrayList<String> al = new ArrayList<String>();
+		for (String key : st.keysWithPrefix("foo")) {
+			al.add(key);
+		}
+		
+		assertEquals(al.size(), 3);
+		assertEquals(al.get(0), "foo");
+		assertEquals(al.get(1), "foobar");
+		assertEquals(al.get(2), "fool");
+	}
+
+	@Test
+	void constructStringTSTAndGetMatchingKeys() {
+		final StringTST<Integer> st = new StringTST<Integer>();
+		st.put("foo", 0);
+		st.put("bar", 1);
+		st.put("baz", 2);
+		st.put("qux", 3);
+		st.put("bap", 3);
+		
+		final ArrayList<String> al = new ArrayList<String>();
+		for (String key : st.keysThatMatch("ba.")) {
+			al.add(key);
+		}
+		
+		assertEquals(al.size(), 3);
+		assertEquals(al.get(0), "bar");
+		assertEquals(al.get(1), "bap");
+		assertEquals(al.get(2), "baz");
+	}
+
+	@Test
+	void constructStringTSTAndGetMatchingKeys2() {
+		final StringTST<Integer> st = new StringTST<Integer>();
+		st.put("foop", 0);
+		st.put("foobar", 1);
+		st.put("fab", 2);
+		st.put("fool", 3);
+		st.put("foam", 3);
+		
+		final ArrayList<String> al = new ArrayList<String>();
+		for (String key : st.keysThatMatch("fo..")) {
+			al.add(key);
+		}
+		
+		assertEquals(al.size(), 3);
+		assertEquals(al.get(0), "foam");
+		assertEquals(al.get(1), "foop");
+		assertEquals(al.get(2), "fool");
+	}
+
+	@Test
+	void constructStringTSTAndGetLongestPrefix() {
+		final StringTST<Integer> st = new StringTST<Integer>();
+		st.put("foop", 0);
+		st.put("foobar", 1);
+		st.put("fab", 2);
+		st.put("fool", 3);
+		st.put("foam", 3);
+		
+		final String lp = st.longestPrefixOf("foopghgjghgjgjgj");
+		assertEquals(lp, "foop");
 	}
 }
