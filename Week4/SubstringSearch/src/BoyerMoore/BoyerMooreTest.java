8150bcf5caccbb897f5fdb900e328af7a0b12488
danielcu888 <appledoughnut@gmail.com>
2018-07-04 22:17:09 +0100

    Week4: Corrected implementation of RabinKarp.Search

--- Week4/SubstringSearch/src/BoyerMoore/BoyerMooreTest.java
+++ Week4/SubstringSearch/src/BoyerMoore/BoyerMooreTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import kmp.KnuthMorrisPratt;
-
 class BoyerMooreTest {
 
 	@Test
--- Week4/SubstringSearch/src/rabinkarp/RabinKarp.java
+++ Week4/SubstringSearch/src/rabinkarp/RabinKarp.java
@@ -2,12 +2,11 @@ package rabinkarp;
 
 public class RabinKarp {
 
-	static final int R = 10;
-	static final char offset = '0';
-	static final int mod = 997;
+	static final int R = 256;
+	static final int mod = 101;
 	
 	private static int charAt(String s, int index) {
-		return s.charAt(index) - RabinKarp.offset;
+		return s.charAt(index);
 	}
 		
 	public static int search(String text, String pattern) {
@@ -22,19 +21,22 @@ public class RabinKarp {
 		int curTextHash = 0;
 		int end = 0;
 		for (end = 0; end <  M; ++end) {
-			patternHash = ((RabinKarp.R * patternHash) + RabinKarp.charAt(pattern, end)) % RabinKarp.mod;
-			curTextHash = ((RabinKarp.R * curTextHash) + RabinKarp.charAt(text, end)) % RabinKarp.mod;
+			patternHash = ((RabinKarp.R * (patternHash % RabinKarp.mod)) + RabinKarp.charAt(pattern, end)) % RabinKarp.mod;
+			curTextHash = ((RabinKarp.R * (curTextHash % RabinKarp.mod)) + RabinKarp.charAt(text, end)) % RabinKarp.mod;
 		}
 		
-		final int c = (int)Math.pow(RabinKarp.R, M-1);
+		// calculate c incrementally to avoid overflow
+		int c = 1;
+		for (int i = 0; i < M-1; ++i) {
+			c = (c % RabinKarp.mod) * RabinKarp.R;
+		}
+		
 		int start;
-		for (start = 0; end < N-1 && curTextHash != patternHash; ++start, ++end) {
-			final int p1 = (RabinKarp.R * curTextHash);
-			final int p2 = (int)(RabinKarp.charAt(text, start) * c);
-			final int p3 = RabinKarp.charAt(text, end);
-			curTextHash = (p1 - p2 + p3) % RabinKarp.mod;
+		for (start = 0; end < N && curTextHash != patternHash; ++start, ++end) {
+			curTextHash = ((curTextHash + RabinKarp.mod - (((int)text.charAt(start) * c) % RabinKarp.mod)) * RabinKarp.R + text.charAt(end)) % RabinKarp.mod;
 		}
 		
 		return patternHash == curTextHash ? start : N;
 	}
 }
+ 
